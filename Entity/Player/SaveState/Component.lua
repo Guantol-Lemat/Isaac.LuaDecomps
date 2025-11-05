172bdc9ad951adbda47376312684be6e1718d066
Guantol-Lemat <guantol.lemat@gmail.com>
2025-11-05 16:34:32 +0100

    fix some collision conditions for slots

---
+++ Entity/Player/SaveState/Component.lua
@@ -0,0 +1,44 @@
+---@class GameStatePlayerComponent
+---@field playerType PlayerType | integer
+---@field controllerIdx integer
+---@field backupOwnerIdx integer -- treated as offset from current state in the playerManager.players list (0 if this is not a backup player)
+---@field parentIdx integer -- index in the playerManager.players list (< 0 if this is not a parent)
+---@field familiarData GameStatePlayer.FamiliarData[]
+---@field familiarCount integer
+
+---@class GameStatePlayer.FamiliarData
+
+--#region Dependencies
+
+
+
+--#endregion
+
+---@class GameStatePlayerComponentModule
+local Module = {}
+
+---@param saveState GameStatePlayerComponent
+local function Init(saveState)
+end
+
+---@return GameStatePlayerComponent
+local function Create()
+    local saveState = {}
+    Init(saveState)
+    return saveState
+end
+
+---@param familiarData GameStatePlayer.FamiliarData
+---@return GameStatePlayer.FamiliarData
+local function FamiliarDataCopy(familiarData)
+end
+
+--#region Module
+
+Module.Create = Create
+Module.Init = Init
+Module.FamiliarDataCopy = FamiliarDataCopy
+
+--#endregion
+
+return Module
